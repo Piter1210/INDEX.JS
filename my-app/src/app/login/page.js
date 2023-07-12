dc3b702869e141da994670e4f9ef82768bf63a2b
Piter1210 <a3522110732@alumno.utehuacan.edu.mx>
2023-07-12 11:36:38 -0600

    Install commit

---
+++ my-app/src/app/login/page.js
@@ -0,0 +1,23 @@
+import "./login.css"
+
+
+export default function login() {
+
+    return (
+    <div style={{
+       display: "flex",
+      flexDirection: "column"
+
+
+
+    } }>
+
+    <imput type="text" placeholder="email" />
+    <imput type="password" placeholder="123456" />
+   <button>login</button>
+
+    </div>
+    )
+
+
+}
